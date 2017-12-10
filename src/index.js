b20292f5475dbaa40a1dd23fbea9bf4a5937b347
y-kurami <yosuke.kurami@gmail.com>
2017-12-11 02:12:29 +0900

    add module path functions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+const path = require('path');
+const fs = require('fs');
+
 const m = require('../build/cv-wasm_node.js');
 const p = new Promise(resolve => {
   const id = setInterval(() => {
@@ -9,9 +12,18 @@ const p = new Promise(resolve => {
   }, 10);
 });
 
-module.exports = function (img1, img2, config, cb) {
+function detectDiff (img1, img2, config, cb) {
   return p.then(m => {
     return m.detectDiff(m, img1, img2, config);
   });
+}
+
+detectDiff.getBrowserJsPath = function() {
+  return path.resolve(__dirname, 'build', 'cv-wasm_browser.js');
 };
 
+detectDiff.getBrowserWasmPath = function() {
+  return path.resolve(__dirname, 'build', 'cv-wasm_browser.wasm');
+};
+
+module.exports = detectDiff;
